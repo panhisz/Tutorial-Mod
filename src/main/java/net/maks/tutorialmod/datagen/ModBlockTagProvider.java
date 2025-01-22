84c33939afae33919920bf0e672f6e9c05ae38d2
Mateusz Kwiecień <mateusz1kwiecien@gmail.com>
2025-01-22 21:45:31 +0100

    minor changes :)

---
+++ src/main/java/net/maks/tutorialmod/datagen/ModBlockTagProvider.java
@@ -0,0 +1,22 @@
+package net.maks.tutorialmod.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.maks.tutorialmod.block.ModBlocks;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.BlockTags;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
+
+    public ModBlockTagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+        getOrCreateTagBuilder(BlockTags.PICKAXE_MINEABLE)
+                .add(ModBlocks.STEELIUM_ORE);
+    }
+}
---
+++ src/main/java/net/maks/tutorialmod/datagen/ModLootTableProvider.java
@@ -0,0 +1,4 @@
+package net.maks.tutorialmod.datagen;
+
+public class ModLootTableProvider {
+}
